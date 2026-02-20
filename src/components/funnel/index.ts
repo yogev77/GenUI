99abfb37115d5fac68e129e317709f063588489c
Yogev Shelly <yogev77@gmail.com>
2026-02-20 13:27:31 +0200

    funnel: remove goeddy.com

--- src/components/funnel/index.ts
+++ src/components/funnel/index.ts
@@ -1,4 +1,1 @@
 // GenFunnel - auto-generated barrel file for funnel page components
-export { default as GoeddyLanding } from './GoeddyLanding';
-export { default as GoeddyDetails } from './GoeddyDetails';
-export { default as GoeddyCheckout } from './GoeddyCheckout';
