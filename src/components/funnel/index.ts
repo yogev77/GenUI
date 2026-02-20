c22c11e98a3d999ed8816265288ea5c3ee5f7647
Yogev Shelly <yogev77@gmail.com>
2026-02-20 14:07:56 +0200

    chore: trigger rebuild 2026-02-20T12:07:54.444Z

--- src/components/funnel/index.ts
+++ src/components/funnel/index.ts
@@ -2,3 +2,4 @@
 export { default as EddyEnglishLanding } from './EddyEnglishLanding';
 export { default as EddyEnglishDetails } from './EddyEnglishDetails';
 export { default as EddyEnglishCheckout } from './EddyEnglishCheckout';
+// rebuild: 2026-02-20T12:07:54.444Z
